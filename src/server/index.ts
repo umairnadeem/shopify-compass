bcd6e22d4fc5c1818a2500880c27cb2562de2645
Umair <nadeem.umair123@gmail.com>
2021-04-10 18:29:44 -0700

    Update export method

--- src/server/index.ts
+++ src/server/index.ts
@@ -4,4 +4,5 @@ require("@babel/register")({
 });
 
 // Import the rest of our application.
-export import server = require("./server");
+ import * as server from "./server";
+ export default server;
